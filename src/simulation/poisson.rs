dc6ec24f5f8649636f2559c5f9442f8106404568
tangxiangong <tangxiangong@gmail.com>
2025-05-14 07:51:12 +0800

    feat: add raw and central moment functions for Poisson processes
    This commit introduces new functions for calculating raw and central moments of first passage times and occupation times in Poisson processes. These additions enhance the statistical analysis capabilities of the simulation module, providing more tools for analyzing stochastic processes.

--- src/simulation/poisson.rs
+++ src/simulation/poisson.rs
@@ -59,8 +59,64 @@ pub fn poisson_fpt(lambda: f64, domain: (f64, f64), max_duration: f64) -> XPyRes
 }
 
 #[pyfunction]
+pub fn poisson_fpt_raw_moment(
+    lambda: f64,
+    domain: (f64, f64),
+    max_duration: f64,
+    order: i32,
+    particles: usize,
+) -> XPyResult<Option<f64>> {
+    let poisson = Poisson::new(lambda)?;
+    let fpt = FirstPassageTime::new(&poisson, domain)?;
+    let result = fpt.raw_moment_p(order, particles, max_duration)?;
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn poisson_fpt_central_moment(
+    lambda: f64,
+    domain: (f64, f64),
+    max_duration: f64,
+    order: i32,
+    particles: usize,
+) -> XPyResult<Option<f64>> {
+    let poisson = Poisson::new(lambda)?;
+    let fpt = FirstPassageTime::new(&poisson, domain)?;
+    let result = fpt.central_moment_p(order, particles, max_duration)?;
+    Ok(result)
+}
+
+#[pyfunction]
 pub fn poisson_occupation_time(lambda: f64, domain: (f64, f64), duration: f64) -> XPyResult<f64> {
     let poisson = Poisson::new(lambda)?;
     let result = poisson.occupation_time(domain, duration)?;
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn poisson_occupation_time_raw_moment(
+    lambda: f64,
+    domain: (f64, f64),
+    duration: f64,
+    order: i32,
+    particles: usize,
+) -> XPyResult<f64> {
+    let poisson = Poisson::new(lambda)?;
+    let oc = OccupationTime::new(&poisson, domain, duration)?;
+    let result = oc.raw_moment_p(order, particles)?;
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn poisson_occupation_time_central_moment(
+    lambda: f64,
+    domain: (f64, f64),
+    duration: f64,
+    order: i32,
+    particles: usize,
+) -> XPyResult<f64> {
+    let poisson = Poisson::new(lambda)?;
+    let oc = OccupationTime::new(&poisson, domain, duration)?;
+    let result = oc.central_moment_p(order, particles)?;
     Ok(result)
 }
