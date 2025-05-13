493651064bc135337bf1483b848d658b44188e00
tangxiangong <tangxiangong@gmail.com>
2025-05-13 08:48:25 +0800

    feat: add functions for raw and central moments of CTRW and occupation time
    This commit introduces new functions for calculating raw and central moments of continuous time random walks (CTRW) and occupation time, enhancing the statistical analysis capabilities of the simulation module. These additions improve the overall functionality and provide more tools for analyzing stochastic processes.

--- src/simulation/ctrw.rs
+++ src/simulation/ctrw.rs
@@ -73,6 +73,38 @@ pub fn ctrw_fpt(
 }
 
 #[pyfunction]
+pub fn ctrw_fpt_raw_moment(
+    alpha: f64,
+    beta: f64,
+    start_position: f64,
+    domain: (f64, f64),
+    order: i32,
+    particles: usize,
+    max_duration: f64,
+) -> XPyResult<Option<f64>> {
+    let ctrw = CTRW::new(alpha, beta, start_position)?;
+    let fpt = FirstPassageTime::new(&ctrw, domain)?;
+    let result = fpt.raw_moment_p(order, particles, max_duration)?;
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn ctrw_fpt_central_moment(
+    alpha: f64,
+    beta: f64,
+    start_position: f64,
+    domain: (f64, f64),
+    order: i32,
+    particles: usize,
+    max_duration: f64,
+) -> XPyResult<Option<f64>> {
+    let ctrw = CTRW::new(alpha, beta, start_position)?;
+    let fpt = FirstPassageTime::new(&ctrw, domain)?;
+    let result = fpt.central_moment_p(order, particles, max_duration)?;
+    Ok(result)
+}
+
+#[pyfunction]
 pub fn ctrw_occupation_time(
     alpha: f64,
     beta: f64,
@@ -82,5 +114,37 @@ pub fn ctrw_occupation_time(
 ) -> XPyResult<f64> {
     let ctrw = CTRW::new(alpha, beta, start_position)?;
     let result = ctrw.occupation_time(domain, duration)?;
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn ctrw_occupation_time_raw_moment(
+    alpha: f64,
+    beta: f64,
+    start_position: f64,
+    domain: (f64, f64),
+    duration: f64,
+    order: i32,
+    particles: usize,
+) -> XPyResult<f64> {
+    let ctrw = CTRW::new(alpha, beta, start_position)?;
+    let oc = OccupationTime::new(&ctrw, domain, duration)?;
+    let result = oc.raw_moment_p(order, particles)?;
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn ctrw_occupation_time_central_moment(
+    alpha: f64,
+    beta: f64,
+    start_position: f64,
+    domain: (f64, f64),
+    duration: f64,
+    order: i32,
+    particles: usize,
+) -> XPyResult<f64> {
+    let ctrw = CTRW::new(alpha, beta, start_position)?;
+    let oc = OccupationTime::new(&ctrw, domain, duration)?;
+    let result = oc.central_moment_p(order, particles)?;
     Ok(result)
 }
