0196d9daee5afaef1ed3070e5ec559d29d36137a
tangxiangong <tangxiangong@gmail.com>
2025-05-13 08:48:35 +0800

    feat: add new functions for raw and central moments of fractional Brownian motion and occupation time
    This commit introduces additional functions for calculating raw and central moments of fractional Brownian motion and occupation time, enhancing the statistical analysis capabilities of the simulation module. The new functions also include time-averaged mean square displacement (TAMSD) and expected time-averaged mean square displacement (E-TAMSD), further improving the functionality of the simulation tools.

--- src/simulation/fbm.rs
+++ src/simulation/fbm.rs
@@ -60,6 +60,38 @@ pub fn fbm_fpt(
 }
 
 #[pyfunction]
+pub fn fbm_fpt_raw_moment(
+    start_position: f64,
+    hurst_exponent: f64,
+    domain: (f64, f64),
+    order: i32,
+    particles: usize,
+    step_size: f64,
+    max_duration: f64,
+) -> XPyResult<Option<f64>> {
+    let fbm = Fbm::new(start_position, hurst_exponent)?;
+    let fpt = FirstPassageTime::new(&fbm, domain)?;
+    let result = fpt.raw_moment(order, particles, max_duration, step_size)?;
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn fbm_fpt_central_moment(
+    start_position: f64,
+    hurst_exponent: f64,
+    domain: (f64, f64),
+    order: i32,
+    particles: usize,
+    step_size: f64,
+    max_duration: f64,
+) -> XPyResult<Option<f64>> {
+    let fbm = Fbm::new(start_position, hurst_exponent)?;
+    let fpt = FirstPassageTime::new(&fbm, domain)?;
+    let result = fpt.central_moment(order, particles, max_duration, step_size)?;
+    Ok(result)
+}
+
+#[pyfunction]
 pub fn fbm_occupation_time(
     start_position: f64,
     hurst_exponent: f64,
@@ -69,5 +101,66 @@ pub fn fbm_occupation_time(
 ) -> XPyResult<f64> {
     let fbm = Fbm::new(start_position, hurst_exponent)?;
     let result = fbm.occupation_time(domain, duration, step_size)?;
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn fbm_occupation_time_raw_moment(
+    start_position: f64,
+    hurst_exponent: f64,
+    domain: (f64, f64),
+    order: i32,
+    particles: usize,
+    step_size: f64,
+    duration: f64,
+) -> XPyResult<f64> {
+    let fbm = Fbm::new(start_position, hurst_exponent)?;
+    let oc = OccupationTime::new(&fbm, domain, duration)?;
+    let result = oc.raw_moment(order, particles, step_size)?;
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn fbm_occupation_time_central_moment(
+    start_position: f64,
+    hurst_exponent: f64,
+    domain: (f64, f64),
+    order: i32,
+    particles: usize,
+    step_size: f64,
+    duration: f64,
+) -> XPyResult<f64> {
+    let fbm = Fbm::new(start_position, hurst_exponent)?;
+    let oc = OccupationTime::new(&fbm, domain, duration)?;
+    let result = oc.central_moment(order, particles, step_size)?;
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn fbm_tamsd(
+    start_position: f64,
+    hurst_exponent: f64,
+    duration: f64,
+    delta: f64,
+    step_size: f64,
+    quad_order: usize,
+) -> XPyResult<f64> {
+    let fbm = Fbm::new(start_position, hurst_exponent)?;
+    let result = fbm.tamsd(duration, delta, step_size, quad_order)?;
+    Ok(result)
+}
+
+#[pyfunction]
+pub fn fbm_eatamsd(
+    start_position: f64,
+    hurst_exponent: f64,
+    duration: f64,
+    delta: f64,
+    particles: usize,
+    step_size: f64,
+    quad_order: usize,
+) -> XPyResult<f64> {
+    let fbm = Fbm::new(start_position, hurst_exponent)?;
+    let result = fbm.eatamsd(duration, delta, particles, step_size, quad_order)?;
     Ok(result)
 }
