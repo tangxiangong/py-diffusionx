12ea6f640296d79e154ac634b6d7875ce440ea7e
tangxiangong <tangxiangong@gmail.com>
2025-05-14 09:28:44 +0800

    feat: enhance simulation module with additional stochastic process simulations
    This commit introduces new modules for simulating various stochastic processes, including Brownian bridge, Brownian excursion, Brownian meander, Cauchy, Gamma, Geometric Brownian motion, Lévy walk, and Ornstein-Uhlenbeck processes. These additions expand the simulation module's capabilities, providing more tools for analyzing complex stochastic processes.

--- src/simulation.rs
+++ src/simulation.rs
@@ -46,3 +46,27 @@ pub use ctrw::*;
 
 mod langevin;
 pub use langevin::*;
+
+mod brownian_bridge;
+pub use brownian_bridge::*;
+
+mod brownian_excursion;
+pub use brownian_excursion::*;
+
+mod brownian_meander;
+pub use brownian_meander::*;
+
+mod cauchy;
+pub use cauchy::*;
+
+mod gamma;
+pub use gamma::*;
+
+mod geometric_bm;
+pub use geometric_bm::*;
+
+mod levy_walk;
+pub use levy_walk::*;
+
+mod ou;
+pub use ou::*;
