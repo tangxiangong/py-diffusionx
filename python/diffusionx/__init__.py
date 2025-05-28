25a63206b52b37eeb4ed31df00fa064924272ff5
tangxiangong <tangxiangong@gmail.com>
2025-05-29 07:47:50 +0800

    fix: update __init__.py to include distribution module in exports
    This commit adds the 'distribution' module to the __all__ list in __init__.py, ensuring it is properly exported and accessible for users of the diffusionx package.

--- python/diffusionx/__init__.py
+++ python/diffusionx/__init__.py
@@ -1,10 +1,12 @@
 from .types import DType
 from . import random
 from . import simulation
+from . import distribution
 
 
 __all__ = [
     "random",
+    "distribution",
     "DType",
     "simulation",
 ]
